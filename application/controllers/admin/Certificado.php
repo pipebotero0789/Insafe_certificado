4dd5ea1cb3602e51046a1a6c35a07a810c9553c4
pipebotero0789 <pipe0789@gmail.com>
2018-06-14 11:19:07 -0500

    subida de archivos y guardado en base de datos

--- application/controllers/admin/Certificado.php
+++ application/controllers/admin/Certificado.php
@@ -36,4 +36,39 @@ class Certificado extends MY_Controller {
         );
         $this->load->view('admin/certificado_view',$dataSend);
     }
+
+    public function subirArchivo(){
+        $config = array(
+            'upload_path'   => FCPATH.'upload/',
+            'allowed_types' => 'xls|csv'
+        );
+        $this->load->library('upload', $config);
+        if ($this->upload->do_upload('file')) {
+            $data = $this->upload->data();
+            @chmod($data['full_path'], 0777);   
+            $this->load->library('Spreadsheet_Excel_Reader');
+            $this->spreadsheet_excel_reader->setOutputEncoding('CP1251');
+            $this->spreadsheet_excel_reader->read($data['full_path']);
+            $sheets = $this->spreadsheet_excel_reader->sheets[0];
+            error_reporting(0);
+            $data_excel = array();
+            for ($i = 2; $i <= $sheets['numRows']; $i++) {
+                if ($sheets['cells'][$i][1] == '') break;
+                $data_excel[$i - 1]['certificado_nombre']    = $sheets['cells'][$i][1];
+                $data_excel[$i - 1]['certificado_cedula']   = $sheets['cells'][$i][2];
+                $data_excel[$i - 1]['certificado_numero'] = $sheets['cells'][$i][3];
+                $data_excel[$i - 1]['certificado_expedicion'] = $sheets['cells'][$i][4];
+                $data_excel[$i - 1]['certificado_vencimiento'] = $sheets['cells'][$i][5];
+                $data_excel[$i - 1]['estado_id'] = $sheets['cells'][$i][6];
+            }
+            $this->db->insert_batch('produccion_certificado', $data_excel);
+            // @unlink($data['full_path']);
+            $mensaje = "el archivo se subio correctamente";
+            $this->index($mensaje);
+        }else{
+            $mensaje = "ocurrio un problema";
+            $this->index($mensaje);
+        }
+    }
+
 }
--- application/views/admin/certificado_view.php
+++ application/views/admin/certificado_view.php
@@ -9,13 +9,18 @@
                         <div class="white-box">
                             <h3 class="box-title m-b-0">Subir Certificado</h3>
                             <p class="text-muted m-b-30 font-13"> Recuerde subir un archivo excel</p>
-                            <form class="form-horizontal">
+                            <form class="form-horizontal" enctype="multipart/form-data" action="<?php echo base_url()?>admin/Certificado/subirArchivo" method="post">
                                 <div class="form-group">
-                                    <label class="col-sm-12">File upload</label>
+                                    <label class="col-sm-12">Subir Archivo</label>
                                     <div class="col-sm-12">
                                         <div class="fileinput fileinput-new input-group" data-provides="fileinput">
-                                            <div class="form-control" data-trigger="fileinput"> <i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div> <span class="input-group-addon btn btn-default btn-file"> <span class="fileinput-new">Select file</span> <span class="fileinput-exists">Change</span>
-                                            <input type="file" name="..."> </span> <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a> </div>
+                                            <div class="form-control" data-trigger="fileinput"> <i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div> <span class="input-group-addon btn btn-default btn-file"> <span class="fileinput-new">Selecciona el archivo</span> <span class="fileinput-exists">Cambiar</span>
+                                            <input type="file" name="file"> </span> <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Cancelar</a> </div>
+                                    </div>
+                                    <div class="form-group m-b-0">
+                                        <div class="col-sm-offset-3 col-sm-9">
+                                            <button type="submit" class="btn btn-info waves-effect waves-light m-t-10">Guardar</button>
+                                        </div>
                                     </div>
                                 </div>
                             </form>

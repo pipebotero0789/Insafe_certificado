00ffb20075c00ce7014673760d171a9311798ff0
pipebotero0789 <pipe0789@gmail.com>
2018-06-18 17:34:01 -0500

    maquetacion final de la carta de PDF

--- application/controllers/Consulta.php
+++ application/controllers/Consulta.php
@@ -85,8 +85,118 @@ class Consulta extends MY_Controller {
 		$this->load->view('consulta/footer_view');
 	}
 
-	public function imprimir(){
-		$this->load->view('carta_view');
+	public function imprimir($id = NULL){
+		$where = array(
+			'certificado_id' => $id
+		);
+		$certificados2 = $this->Crud_certificado->GetDatos($where);
+		$fechaRegistro = $this->fechas($certificados2[0]->certificado_expedicion);
+		$hoy = getdate();
+		$fechaHoy = $this->acomodarFecha($hoy);
+		$datos = array(
+			'certificados' => $certificados2,
+			'fechaExp' => $fechaRegistro,
+			'fechaHoy' => $fechaHoy
+		);
+		$this->load->view('carta_view', $datos);
 	}
+
+	public function fechas($date) { 
+	    	$jour=substr($date, 0, 2); 
+	        $mois=substr($date, 3, 2); 
+	        $annee=substr($date, 6, 4);
+	        switch ($mois) {
+	         	case '1':
+	         		$mes = 'enero';
+	         		break;
+	         	case '2':
+	         		$mes = 'febrero';
+	         		break;
+	         	case '3':
+	         		$mes = 'marzo';
+	         		break;
+	         	case '4':
+	         		$mes = 'abril';
+	         		break;
+	         	case '5':
+	         		$mes = 'mayo';
+	         		break;
+	         	case '6':
+	         		$mes = 'junio';
+	         		break;
+	         	case '7':
+	         		$mes = 'julio';
+	         		break;
+	         	case '8':
+	         		$mes = 'agosto';
+	         		break;
+	         	case '9':
+	         		$mes = 'septiembre';
+	         		break;
+	         	case '10':
+	         		$mes = 'octubre';
+	         		break;
+	         	case '11':
+	         		$mes = 'noviembre';
+	         		break;
+	         	case '12':
+	         		$mes = 'diciembre';
+	         		break;			
+	         	default:
+	         		# code...
+	         		break;
+	        } 
+	        $fechaTexto = 'fecha de emisión a los '.$jour.' días del mes de '.$mes.' del '.$annee;
+	        return $fechaTexto;	
+    }
+
+    public function acomodarFecha($date){
+    	$jour= $date['mday']; 
+	    $mois= $date['mon']; 
+	    $annee= $date['year'];
+	    switch ($mois) {
+	         	case '1':
+	         		$mes = 'enero';
+	         		break;
+	         	case '2':
+	         		$mes = 'febrero';
+	         		break;
+	         	case '3':
+	         		$mes = 'marzo';
+	         		break;
+	         	case '4':
+	         		$mes = 'abril';
+	         		break;
+	         	case '5':
+	         		$mes = 'mayo';
+	         		break;
+	         	case '6':
+	         		$mes = 'junio';
+	         		break;
+	         	case '7':
+	         		$mes = 'julio';
+	         		break;
+	         	case '8':
+	         		$mes = 'agosto';
+	         		break;
+	         	case '9':
+	         		$mes = 'septiembre';
+	         		break;
+	         	case '10':
+	         		$mes = 'octubre';
+	         		break;
+	         	case '11':
+	         		$mes = 'noviembre';
+	         		break;
+	         	case '12':
+	         		$mes = 'diciembre';
+	         		break;			
+	         	default:
+	         		# code...
+	         		break;
+	        } 
+	        $fechaTexto = 'Este documento se realiza a los '.$jour.' días del mes de '.$mes.' del '.$annee;
+	        return $fechaTexto;	
+    }
 
 }
--- application/views/carta_view.php
+++ application/views/carta_view.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js"></script>
 </head>
 <body id="funciona" name="funciona">
 <table class="main k-table" data-type="image" id="66b53801-5799-cbbb-5362-ba9c90a43599" style="background-color: #ffffff; display: table;" width="1100" cellspacing="0" cellpadding="0" border="0" align="center">
@@ -32,7 +31,7 @@
 <table class="main k-table" data-type="image" id="66b53801-5799-cbbb-5362-ba9c90a43599" style="background-color: #ffffff; display: table;" width="1100" cellspacing="0" cellpadding="0" border="0" align="center">
 <tbody>
 <tr>
-<td class="image" style="padding: 0px 0px; display: block;width: 1100px;height: 130px; font-family: arial" align="justify">Qué JENNYFER PAOLA BENAVIDES NAVARRETE identificado(a) con cédula de ciudadanía No.  1.014.228.853,  realizó  y  aprobó  el  curso  de  PERSONA  COMPETENTE  PARA  LA REVISIÓN  E  INSPECCIÓN  DE  LOS  EQUIPOS  DE PROTECCIÓN  CONTRA  CAÍDAS  DE  LA MARCA INSAFE FALL PROTECCIÓN, número de certificado RPQ-20-XXX, fecha de emisión a los 29 días del mes de mayo de l 2018.</td>
+<td class="image" style="padding: 0px 0px; display: block;width: 1100px;height: 130px; font-family: arial" align="justify">Qué <span style="text-transform: uppercase;"><?php echo $certificados[0]->certificado_nombre ?></span> identificado(a) con cédula de ciudadanía No. <span style="text-transform: uppercase;"><?php echo $certificados[0]->certificado_cedula ?></span>,  realizó  y  aprobó  el  curso  de  PERSONA  COMPETENTE  PARA  LA REVISIÓN  E  INSPECCIÓN  DE  LOS  EQUIPOS  DE PROTECCIÓN  CONTRA  CAÍDAS  DE  LA MARCA INSAFE FALL PROTECCIÓN, número de certificado <span style="text-transform: uppercase;"><?php echo $certificados[0]->certificado_numero ?></span>, <span><?php echo $fechaExp ?></span>.</td>
 </tr>
 </tbody>
 </table>
@@ -40,7 +39,12 @@
 <table class="main k-table" data-type="image" id="66b53801-5799-cbbb-5362-ba9c90a43599" style="background-color: #ffffff; display: table;" width="1100" cellspacing="0" cellpadding="0" border="0" align="center">
 <tbody>
 <tr>
-<td class="image" style="padding: 0px 0px; display: block;width: 1100px;height: 60px; font-family: arial" align="justify">ESTADO: VIGENTE.</td>
+<td class="image" style="padding: 0px 0px; display: block;width: 1100px;height: 60px; font-family: arial" align="justify">ESTADO: <span style="text-transform: uppercase;">
+	<?php if($certificados[0]->estado_id == 1){ 
+		echo 'Vigente'; 
+	}else{ 
+		echo 'Vencido'; 
+	} ?></span>.</td>
 </tr>
 </tbody>
 </table>
@@ -48,7 +52,7 @@
 <table class="main k-table" data-type="image" id="66b53801-5799-cbbb-5362-ba9c90a43599" style="background-color: #ffffff; display: table;" width="1100" cellspacing="0" cellpadding="0" border="0" align="center">
 <tbody>
 <tr>
-<td class="image" style="padding: 0px 0px; display: block;width: 1100px;height: 130px; font-family: arial" align="justify">Este documento se realiza a los 29 días del mes de mayo del 2018.</td>
+<td class="image" style="padding: 0px 0px; display: block;width: 1100px;height: 130px; font-family: arial" align="justify"><span> <?php echo $fechaHoy ?> </span> </td>
 </tr>
 </tbody>
 </table>
@@ -72,48 +76,5 @@
 </tr>
 </tbody>
 </table>
-<button onclick="demoFromHTML()">PDF</button>
 </body>
-
-<script type="text/javascript">
-        function demoFromHTML() {
-            var pdf = new jsPDF('p', 'pt', 'letter');
-            // source can be HTML-formatted string, or a reference
-            // to an actual DOM element from which the text will be scraped.
-            source = $('#customers')[0];
-
-            // we support special element handlers. Register them with jQuery-style 
-            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-            // There is no support for any other type of selectors 
-            // (class, of compound) at this time.
-            specialElementHandlers = {
-                // element with id of "bypass" - jQuery style selector
-                '#bypassme': function(element, renderer) {
-                    // true = "handled elsewhere, bypass text extraction"
-                    return true
-                }
-            };
-            margins = {
-                top: 80,
-                bottom: 60,
-                left: 40,
-                width: 522
-            };
-            // all coords and widths are in jsPDF instance's declared units
-            // 'inches' in this case
-            pdf.fromHTML(
-                    source, // HTML string or DOM elem ref.
-                    margins.left, // x coord
-                    margins.top, {// y coord
-                        'width': margins.width, // max width of content on PDF
-                        'elementHandlers': specialElementHandlers
-                    },
-            function(dispose) {
-                // dispose: object with X, Y of the last line add to the PDF 
-                //          this allow the insertion of new lines after html
-                pdf.save('Test.pdf');
-            }
-            , margins);
-        }
-    </script>
 </html>
